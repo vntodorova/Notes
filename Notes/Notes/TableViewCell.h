4986fa43f401244068d1f321512326907a7396b8
vntodorova <veneta95@gmail.com>
2017-03-30 17:02:26 +0300

    Added table view

---
+++ Notes/Notes/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  Notes
+//
+//  Created by Nemetschek A-Team on 3/30/17.
+//  Copyright © 2017 Nemetschek DreamTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
