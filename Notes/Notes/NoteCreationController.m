8b18ba95c713dded01254cc81d56670a5e62011c
IvanDimitrovCode <ivan_dimitrov.work@yahoo.com>
2017-03-31 11:52:15 +0300

    Added buttons animations

--- Notes/Notes/NoteCreationController.m
+++ Notes/Notes/NoteCreationController.m
@@ -7,8 +7,10 @@
 //
 
 #import "NoteCreationController.h"
+#import "Defines.h"
 
 @interface NoteCreationController ()
+@property NSMutableArray *hiddenButtonsList;
 
 @end
 
@@ -16,25 +18,81 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.hiddenButtonsList = [[NSMutableArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)onOptionsClick:(UIButton *)sender
+{
+    if(self.hiddenButtonsList.count > 0)
+    {
+        [self destroyHiddenButtonsOnBaseButton:(UIButton *)sender];
+    }
+    else
+    {
+        [self createHiddenButtons:sender];
+    }
 }
-*/
 
-- (IBAction)onOptionsClick:(id)sender {
+- (void)destroyHiddenButtonsOnBaseButton:(UIButton *)sender
+{
+    for (UIButton *button in self.hiddenButtonsList) {
+        [UIView animateWithDuration:0.5
+                         animations:^
+        {
+            button.frame = CGRectMake(sender.frame.origin.x + SMALL_BUTTON_DISTANCE / 2,
+                                      sender.frame.origin.y + SMALL_BUTTON_DISTANCE / 2,
+                                      0,
+                                      0);
+        }
+         completion:^(BOOL finished) {
+             [button removeFromSuperview];
+         }];
+    }
+    [self.hiddenButtonsList removeAllObjects];
+}
+
+- (void)createHiddenButtons:(UIButton *)sender
+{
+    long optionsButtonX = sender.frame.origin.x - SMALL_BUTTON_DISTANCE;
+    long optionsButtonY = sender.frame.origin.y - SMALL_BUTTON_DISTANCE;
+    [self createButtonWithX:optionsButtonX andY:optionsButtonY andBaseButton:sender andImage:[UIImage imageNamed:@"camera.png"]];
+    
+    optionsButtonX = sender.frame.origin.x - SMALL_BUTTON_DISTANCE;
+    optionsButtonY = sender.frame.origin.y;
+    [self createButtonWithX:optionsButtonX andY:optionsButtonY andBaseButton:sender andImage:[UIImage imageNamed:@"drawing.png"]];
+    
+    optionsButtonX = sender.frame.origin.x;
+    optionsButtonY = sender.frame.origin.y - SMALL_BUTTON_DISTANCE;
+    [self createButtonWithX:optionsButtonX andY:optionsButtonY andBaseButton:sender andImage:[UIImage imageNamed:@"list.png"]];
+}
+
+- (void)createButtonWithX:(long) xCoordinates andY:(long) yCoordinates andBaseButton:(UIButton *)sender andImage:(UIImage*)image
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button addTarget:self action:@selector(aMethod) forControlEvents:UIControlEventTouchUpInside];
+    
+    button.frame = CGRectMake(sender.frame.origin.x + SMALL_BUTTON_DISTANCE / 2,
+                              sender.frame.origin.y + SMALL_BUTTON_DISTANCE / 2,
+                              0,
+                              0);
+    
+    [UIView animateWithDuration:0.5
+                     animations:^
+     {
+         [button setImage:image forState:UIControlStateNormal];
+         button.frame = CGRectMake(xCoordinates, yCoordinates, SMALL_BUTTONS_WIDTH, SMALL_BUTTONS_HEIGHT);
+         [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+         [self.view addSubview:button];
+         [self.hiddenButtonsList addObject:button];
+     }];
+}
+
+- (void)aMethod{
+    
 }
 
 - (IBAction)onCreateClick:(id)sender {
@@ -43,7 +101,7 @@
     self.note.body = self.noteBody.text;
 }
 
--(NSArray *) getTagsFromText:(NSString *) tags
+- (NSArray *)getTagsFromText:(NSString *) tags
 {
     return nil;
 }
